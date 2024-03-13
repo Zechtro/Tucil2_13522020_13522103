836f07291a5ddcce251ab482c2b86d5a7731a1ea
AureliusJustin <88382259+AureliusJustin@users.noreply.github.com>
2024-03-13 19:48:50 +0700

    fix: bezierN
    reverse list right sebelum di rekursi

--- bezierN.py
+++ bezierN.py
@@ -9,7 +9,6 @@ def BezierN(points, iterasi, iterasiMax):
         return []
     else:
         q = points
-        print(q)
         left = [q[0]]
         right = [q[-1]]
         while len(q) > 1:
@@ -17,7 +16,7 @@ def BezierN(points, iterasi, iterasiMax):
             q = [getMidPoint(temp[i], temp[i+1]) for i in range(len(temp)-1)]
             left.append(q[0])
             right.append(q[-1])
-            print(q)
+        right.reverse()
         if iterasi == 0:
             iterasi += 1
             return [points[0]] + BezierN(left, iterasi, iterasiMax) + [left[-1]] + BezierN(right, iterasi, iterasiMax) + [points[-1]]
